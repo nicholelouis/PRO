d49a75ea9f613a7fed63a8c00dcaf4f09051748f
nicholelouis <nicholelouiss@gmail.com>
2024-05-11 14:48:57 +0100

    project structure

---
+++ PokerProject/game.py
@@ -0,0 +1,12 @@
+class Game:
+
+    def __init__(self, players, common_cards, private_cards):
+        self.players = players
+        self.common_cards = common_cards
+        self.private_cards = private_cards
+
+    def get_winner(
+    players: list[Player],
+    common_cards: list[Card],
+    private_cards: list[list[Card]],
+    return [Player | None, Hand]
